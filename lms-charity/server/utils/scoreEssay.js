a592c86e646a501d5877bc6b0da78bafc3e173ee
Luqman Booso <131631632+luqmanbooso@users.noreply.github.com>
2025-07-22 19:55:06 +0530

    second secret commit shhhhhhhhh

--- lms-charity/server/utils/scoreEssay.js
+++ lms-charity/server/utils/scoreEssay.js
@@ -3,7 +3,12 @@ import axios from 'axios';
 export default async function scoreEssay(essayText) {
   try {
     const response = await axios.post('http://127.0.0.1:5000/score', { essay: essayText });
-    return response.data;
+    // The ML response is an array like [{ label: 'POSITIVE', score: 0.999 }]
+    const ml = Array.isArray(response.data) ? response.data[0] : response.data;
+    return {
+      score: ml.score,
+      feedback: ml.label
+    };
   } catch (error) {
     console.error(error);
     return { error: "Scoring failed" };
